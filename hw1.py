869f89e7251cbe171633f2aaac8e74a315c7a855
eminsakk <e238083@metu.edu.tr>
2022-10-21 01:00:39 +0300

    input parsing done

--- hw1.py
+++ hw1.py
@@ -1,7 +1,50 @@
+def parseInput(file_name):
+    # This function parses the input file and return as it to a dictionary.
+
+    # Get the minimum number of customer to be arrived
+    rawInput = open(file_name, "r").read()
+    startIdx = rawInput.find(" ") + 1
+    digitSize = rawInput[startIdx:].find(",")
+    neededCustomer = int(rawInput[startIdx:startIdx + digitSize])
+    # Get the grid as a list
+    startIdx = startIdx + digitSize + 8
+
+    gridString = rawInput[startIdx:len(rawInput) - 1]
+    
+    grid = []
+    flag = False    
+    tmpStr = ''
+
+    for char in gridString:
+        if char == "'" and len(tmpStr) == 0:
+            flag = True
+            continue
+        elif char == "'" and len(tmpStr) != 0:
+            flag = False
+            grid.append(tmpStr)
+            tmpStr = ''
+        if flag:
+            tmpStr += char
+    
+    parsedInput = { "min" : neededCustomer, "env": grid}
+    return parsedInput
+    
+    
 
 
-def main():
-    pass
+def UnInformedSearch(method_name,problem_file_name):
+    dictionary = parseInput(problem_file_name)
+    print(dictionary)
 
-if __name__ == "__main__":
-    main()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')
+    # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    UnInformedSearch("DFS", "sampleproblem.txt")
