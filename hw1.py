d6cf41906d75a99cd2552c06743e0b275dfb4a23
eminsakk <e238083@metu.edu.tr>
2022-10-22 17:23:05 +0300

    graph and node class finished

--- hw1.py
+++ hw1.py
@@ -1,3 +1,48 @@
+class Node:
+    def __init__(self,nodeName,edges):
+        self.nodeName = nodeName
+        self.edges = edges
+    def getEdges(self):
+        return self.edges
+class Graph:
+    nodeList = []
+    customerList = []
+    def __init__(self,grid):
+        for i in range(0,len(grid)):
+            row = grid[i]
+            for j in range(0,len(row)):
+                nodeName = (i,j)
+                # Add Edges
+                edges = self.createEdges(len(grid),len(row),i,j)
+                tmpNode = Node(nodeName, edges)
+                self.nodeList.append(tmpNode)
+        for node in self.nodeList:
+            xPos = node.nodeName[0]
+            yPos = node.nodeName[1]
+            if grid[xPos][yPos] == 'C':
+                self.customerList.append(node)
+            elif grid[xPos][yPos] == 'S':
+                self.startNode = node
+            elif grid[xPos][yPos] == 'F':
+                self.endNode = node
+    def printNodes(self):
+        for x in self.nodeList:
+            print(x.nodeName)
+            print(x.getEdges())
+    
+
+    def createEdges(self,rowLen,colLen,currPosX,currPosY):
+        edgeList = []
+        if currPosX  + 1 < rowLen:
+            edgeList.append((currPosX + 1,currPosY))
+        if currPosX - 1 >= 0:
+            edgeList.append((currPosX - 1,currPosY))
+        if currPosY + 1 < colLen:
+            edgeList.append((currPosX,currPosY + 1))
+        if currPosY - 1 >= 0:
+            edgeList.append((currPosX,currPosY - 1))
+        return edgeList
+
 def parseInput(file_name):
     # This function parses the input file and return as it to a dictionary.
 
@@ -29,22 +74,20 @@ def parseInput(file_name):
     parsedInput = { "min" : neededCustomer, "env": grid}
     return parsedInput
     
-    
-
 
 def UnInformedSearch(method_name,problem_file_name):
     dictionary = parseInput(problem_file_name)
-    print(dictionary)
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')
-    # Press Ctrl+F8 to toggle the breakpoint.
-
+    grid = Graph(dictionary["env"])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     UnInformedSearch("DFS", "sampleproblem.txt")
+
+
+# Searching Algorithms Implementations.
+def BFS():
+    pass
+def DFS():
+    pass
+def UCS():
+    pass
