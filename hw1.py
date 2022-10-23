c3aa91e17a83833e4bdad44bf2b2242defbede28
eminsakk <e238083@metu.edu.tr>
2022-10-24 00:29:09 +0300

    graph refactored 1.1

--- hw1.py
+++ hw1.py
@@ -3,6 +3,31 @@ class Node:
         self.nodeName = nodeName
         self.edges = []
 
+    def createEdges(self,edgeList):
+        for n in edgeList:
+            if n.getName() != self.getName() and self.getName()[2] == 'C' and n.getName()[2] != 'S':
+                self.edges.append(n.getName())
+            elif n.getName() != self.getName() and self.getName()[2] == 'S':
+                self.edges.append(n.getName())
+        # To make things easier sort the edge array.
+        #self.edges = self.sortEdges()
+
+    def sortEdges(self):
+        # Quick Sort Algorithm.
+        smallerThanPivot = []
+        biggerThanPivot = []
+        
+        if len(self.edges) <= 1:
+            return self.edges
+        
+        pivot = self.edges[0]
+
+        for edge in self.edges:
+            if abs(edge[0] - self.getName()[0]) < abs(pivot[0] - self.getName()[0]):
+                smallerThanPivot.append(edge)
+            elif abs(edge[1] - self.getName()[1] < abs(pivot[1] - self.getName()[1])):
+                smallerThanPivot.append
+
     # Getter Functions.
     def getEdges(self):
         return self.edges
@@ -11,7 +36,7 @@ class Node:
         return self.nodeName
 
 class Graph:
-    nodeList = []
+    nodeList = [] # [c,s,f,c,c,c,c]
     def __init__(self,grid):
 
         for i in range(0,len(grid)):
@@ -19,21 +44,17 @@ class Graph:
             for j in range(0,len(row)):
                 nodeType = row[j]
                 if nodeType == 'S' or nodeType == 'F' or nodeType == 'C':
-                    nodeName = (i,j)
+                    nodeName = (i,j,nodeType)
                     tmpNode = Node(nodeName)
                     self.nodeList.append(tmpNode)
-        
+
+
         for node in self.nodeList:
-            for toBeAdd in self.nodeList:
-                if node.getName() != toBeAdd.getName():
-                    node.edges.append(toBeAdd.getName())
-
-
+            node.createEdges(self.nodeList)
 
     def printNodes(self):
         for x in self.nodeList:
             print(x.getName())
-            print(x.getEdges())
 
 
 def parseInput(file_name):
@@ -72,15 +93,22 @@ def UnInformedSearch(method_name,problem_file_name):
     dictionary = parseInput(problem_file_name)
     grid = Graph(dictionary["env"])
     grid.printNodes()
+    
+    if method_name == "DFS":
+        return DFS(grid)
+    elif method_name == 'BFS':
+        return BFS(grid)
+    else:
+        return UCS(grid)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     UnInformedSearch("DFS", "sampleproblem.txt")
 
 # Searching Algorithms Implementations.
-def BFS():
+def DFS(grid):
     pass
-def DFS():
+def BFS(grid):
     pass
-def UCS():
+def UCS(grid):
     pass
