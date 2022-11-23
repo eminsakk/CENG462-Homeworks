40a21db7e6a517ff3355cc8969edc7f47758561f
eminsakk <63427668+eminsakk@users.noreply.github.com>
2022-11-23 15:34:14 +0300

    Update hw3_e2380830.py

--- hw3/hw3_e2380830.py
+++ hw3/hw3_e2380830.py
@@ -1,7 +1,39 @@
+class TreeNode:
+    def __init__(self,nodeName):
+        self.nodeName = nodeName
+        self.childrens = []
 
+    def setChildren(self,childrenNodes):
+        self.childrens = list(childrenNodes)
 
+    def getName(self):
+        return self.nodeName
 
+    def getChildrens(self):
+        return self.childrens
 
+def createChildrens(state):
+    childrens = []
+    for i in range(0,len(state)):
+        currNumber = state[i]
+
+        if currNumber == 0:
+            continue
+
+        for j in range(0,currNumber):
+            childNodeName = list(state)
+            childNodeName[i] = j
+            childNode = TreeNode(childNodeName)
+            childrens.append(childNode)
+    return childrens
+
+def createGameTree(input):
+
+    node = TreeNode(input)
+    childrens = createChildrens(node.getName())
+    for child in childrens:
+        node.getChildrens().append(createGameTree(child.getName()))
+    return node
 
 def inputReader(file_name):
     rawInput = open(file_name,"r").read()
@@ -10,7 +42,8 @@ def inputReader(file_name):
 
 def SolveGame(method_name, problem_file_name, player_type):
     input = inputReader(problem_file_name)
-
+    rootNode = createGameTree(input)
+    print("GAME TREE DEBUG POINT!")
 
 if __name__ == "__main__":
     SolveGame("Minimax","nim1.txt","MAX")
