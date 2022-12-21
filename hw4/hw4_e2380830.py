9395c842b3e5390c04e26749d78862ae06645c02
eminsakk <63427668+eminsakk@users.noreply.github.com>
2022-12-21 16:40:02 +0300

    Update hw4_e2380830.py

--- hw4/hw4_e2380830.py
+++ hw4/hw4_e2380830.py
@@ -1,7 +1,9 @@
+import copy
 class BayesNode:
     def __init__(self,name):
         self.nodeName = name
         self.edges = []
+        self.parents = []
         
     def createEdges(self,paths,nodes):
         for path in paths:
@@ -9,28 +11,103 @@ class BayesNode:
             if self.nodeName in parents:
                 node = findByName(path[1], nodes)
                 self.edges.append(node)
-    
+                node.parents = parents
+            
     def getName(self):
         return self.nodeName
 
+    def getParents(self):
+        return self.parents
 
 class BayesNet:
-    def __init__(self,names,paths):
+    def __init__(self,names,paths,table):
         self.nodes = []
+        self.table = None
         for name in names:
             tmp = BayesNode(name)
             self.nodes.append(tmp)
         
         for node in self.nodes:
             node.createEdges(paths,self.nodes)
+        self.table = table
+
+    def findNode(self,name):
+        for item in self.nodes:
+            if item.getName() == name:
+                return item
+        return None 
+
+    def getVars(self):
+        return self.nodes
+    def getSize(self):
+        return len(self.nodes)
+
+    def enumerationInference(self,query):
+        variables = query["variables"]
+        evidences = query["evidences"]
+
+        Qx = []
+        for x_i in [True,False]:
+            tmpQx = self.enumerateAll(x_i,evidences,self.nodes)
+            Qx.append(tmpQx)
+
+
+        return normalizeFindings(Qx)
+
+    def enumerateAll(self,x_i,evidences,nodes):
+
+        if len(nodes) == 0:
+            return 1.0
         
+        copyNodes = copy.copy(nodes)
+        
+        var = copyNodes[0]
+        evidenceKeys = evidences.keys()
+
+
+        if var in evidenceKeys:
+            return self.calcProb(var,x_i,evidences) * self.enumerateAll(self,x_i,evidences,copyNodes[1:])
+    
+    def calcProb(self,var,x_i,evidences):
+        prob = 0
+        parents = var.getParents()
+        if len(parents) == 0:
+            ## No parents.
+            entry = findTableEntry(self.table,var)
+            p = tuple(entry[2])[0]
+            prob = p if evidences[var.getName()] else 1 - p
+
+        
+        else:
+            ## Have at least 1 parents.
+            parentEntries = []
+            
+            for parent in parents:
+                parentEntries.append(findTableEntry(self.table,parent))
+            
+            tmp = []
+
+            for entry in parentEntries:
+                for pair in entry[2].keys():
+                    tmp.append(entry[2][str(pair)])
+
+            
+
+
+        return prob
+
+
+def findTableEntry(table,var):
+    for entry in table:
+        if var.getName() == entry[0]:
+            return entry
+    return None
 
 def findByName(target,container):
     for item in container:
         if item.getName() == target:
             return item
     return None
-
 
 def parseNodeNames(start,end,lines):
     nodes = []
@@ -54,8 +131,6 @@ def parseTable(start,end,lines):
         entry = lines[i].replace('\n','')
         entry = eval(entry)
         table.append(entry)
-    print(table[0])
-    print(table[0][1])
     return table
 
 def parser(problem_file):
@@ -70,19 +145,50 @@ def parser(problem_file):
     paths = parsePaths(pathStartIdx,tableStartIdx,lines)
     table = parseTable(tableStartIdx,queryIdx,lines)
 
+    query = eval(lines[queryIdx])
+    bayesianNet = BayesNet(nodeNames,paths,table)
 
-    bayesianNet = BayesNet(nodeNames,paths)
+    return bayesianNet,query
 
-    return bayesianNet,table
+def queryParser(query):
+    variableNames = [x for x in query if type(x) is not dict]
+    evidences = None
+    for item in query:
+        if type(item) == dict:
+            evidences = item
+    return {"variables": variableNames, "evidences": evidences}
 
+
+
+##### ENUMERATION FUNCTIONS #####
+
+
+def normalizeFindings(l):
+    z = sum(l)
+    return tuple(x * 1/z for x in l)
+
+##### ENUMERATION FUNCTIONS ENDS. #####
 
 
 def DoInference(method_name,problem_file,iteration):
     parsed = parser(problem_file)
 
     bayesianNet = parsed[0]
-    table = parsed[1]
-    pass
+    query = parsed[1]
+    parsedQuery = queryParser(query)
+
+    if method_name == "ENUMERATION":
+        # Inference part.
+        bayesianNet.enumerationInference(parsedQuery)
+
+        pass
+
+    elif method_name == "GIBBS":
+        # Sampling part
+
+        pass
+
+
 
 
 
