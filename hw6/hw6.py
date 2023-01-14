6e88c164d4c707c554940775082ba00e552323c7
eminsakk <63427668+eminsakk@users.noreply.github.com>
2023-01-14 14:02:39 +0300

    Update hw6.py

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -5,21 +5,15 @@ import json
 
 class NetworkNode:
     
-    
-    #Bayesian Network Layer Node
+    #Neural Network Layer Node
     def __init__(self):
         # Initialize the node with random weights
         self.weights = []
         self.value = 0
-        self.delta = 0
-        pass
-    
-    
+        self.delta = 0    
+        
     def __str__(self):
         return "     Weights: " + str(self.weights)
-    
-        
-
 class Network:
     #Create Directed Acyclic Network of given number layers.
 
@@ -46,7 +40,9 @@ class Network:
                 
 
         self.inputLayer = self.layers[0]
-        self.outputLayer = self.layers[len(self.layers) - 1]
+        
+        outIdx = len(self.layers) - 1
+        self.outputLayer = self.layers[outIdx]
         
     
     
@@ -55,6 +51,48 @@ class Network:
         for i in range(len(inputs)):
             self.inputLayer[i].value = inputs[i]
     
+    
+    def forwardPass(self):
+        # Forward pass
+        for layerID,layerNodeList in self.layers.items():
+            if layerID == 0:
+                continue
+            # Node index in the layer.
+            for idx in range(len(layerNodeList)):
+                nodeSum = 0
+                for i in range(len(self.layers[layerID - 1])):
+                    nodeSum += self.layers[layerID - 1][i].weights[idx] * self.layers[layerID - 1][i].value
+
+                layerNodeList[idx].value = sig(nodeSum)
+        return
+    
+    
+    def backwardPass(self,y):
+        # Backward pass using deltas.
+        for idx,node in enumerate(self.outputLayer):
+            node.delta = dsig(node.value) * (y[idx] - node.value)
+        
+
+        for layerID,layerNodeList in reversed(self.layers.items()):
+            if layerID == len(self.layers) - 1:
+                continue
+
+            for node in layerNodeList:
+                sum = 0
+                for i in range(len(self.layers[layerID+1])):
+                    sum += (self.layers[layerID+1][i].delta * node.weights[i])
+                node.delta = dsig(node.value) * sum
+        return
+    
+    def updateWeights(self,learningRate):
+
+        for layerID,layerNodeList in self.layers.items():            
+            for node in layerNodeList:
+                for i in range(len(node.weights)):
+                    node.weights[i] += learningRate * node.delta * node.value
+        return
+
+
     def __str__(self):
         ret = ""
         
@@ -65,67 +103,42 @@ class Network:
                 ret += str(node) + " "
             
         return ret
-        
-    
-    
     
 def sig(x):
     #Sigmoid activation function.
     return 1 / (1+np.exp(-x))    
         
-        
+def dsig(x):
+    # Derrivative of sigmoid activation function.
+    return np.exp(-x) / ((1+np.exp(-x))**2)
     
 
+def mse(y, y_prime):
+    return np.mean((y - y_prime)**2)
+
+
 def BackPropagationLearner(X,y, net, learning_rate, epochs):    
-    # initialize each weight with the values min_value=-0.5, max_value=0.5,
     for epoch in range(epochs):
 		# Iterate over each example in the dataset X,y.
-
         for data in X:
 			# Activate input layer
             net.setInputs(data)
             
 			# Forward pass
-			
-            for layer in net.layers:
-                if layer == 0:
-                    continue
-                else:
-                    for node in net.layers[layer]:
-                        sum = 0
-                        for i in range(len(net.layers[layer-1])):
-                            sum += net.layers[layer-1][i].value * node.weights[i]
-                        node.value = sig(sum)
+            net.forwardPass()
             
-    
-			# Error for the MSE cost function
-            for i in range(len(net.outputLayer)):
-                net.outputLayer[i].delta = (net.outputLayer[i].value - y[i]) * net.outputLayer[i].value * (1 - net.outputLayer[i].value)
+            # Error for the MSE cost function
+            
 
-            for layer in reversed(net.layers):
-                if layer == len(net.layers) - 1:
-                    continue
-                else:
-                    for node in net.layers[layer]:
-                        sum = 0
-                        for i in range(len(net.layers[layer+1])):
-                            sum += net.layers[layer+1][i].delta * node.weights[i]
-                        node.delta = node.value * (1 - node.value) * sum
-            
-            for layer in net.layers:
-                for node in net.layers[layer]:
-                    for i in range(len(node.weights)):
-                        node.weights[i] += learning_rate * net.layers[layer-1][i].value * node.delta
-            
-			# The activation function used is sigmoid function
-			
-			# Backward pass
+            # Propagate deltas backward from output layer to input layer.
+            net.backwardPass(y)
+
 
 			#  Update weights
-        pass
+            net.updateWeights(learning_rate)
 
-		
 
+    # return the trained network
     return net
 
 
@@ -143,35 +156,29 @@ def NeuralNetLearner(X,y, hidden_layer_sizes=None, learning_rate=0.01, epochs=10
     
 
 
-    layers = [4,3,3]
-    net = Network(layers)
-    print(net)
-    BackPropagationLearner(X,y, net, learning_rate, epochs)
+    layers = [len(X[0])]
+
+    if hidden_layer_sizes is None:
+        layers.append(3)
+    else:
+        layers += hidden_layer_sizes
+        
+    layers.append(np.unique(y).shape[0])
+
+
+    firstNet = Network(layers)
+    lastNet = BackPropagationLearner(X,y, firstNet, learning_rate, epochs)
 
 
     def predict(example):
-        # activate input layer
-
-        
-        
-
         # set input layer values to example values
-        net.setInputs(example)
-        
-        
+        lastNet.setInputs(example)
+    
         # forward pass
-        for layer in net.layers:
-            if layer == 0:
-                continue
-            else:
-                for node in net.layers[layer]:
-                    sum = 0
-                    for i in range(len(net.layers[layer-1])):
-                        sum += net.layers[layer-1][i].value * node.weights[i]
-                    node.value = sig(sum)
-                    
+        lastNet.forwardPass()
+        
         # find the max node from output nodes 
-        prediction = net.outputLayer.index(max(net.outputLayer, key=lambda x: x.value))
+        prediction = lastNet.outputLayer.index(max(lastNet.outputLayer, key=lambda x: x.value))
         return prediction
 
     return predict
@@ -184,6 +191,6 @@ iris = datasets.load_iris()
 X = iris.data  
 y = iris.target
 
-nNL = NeuralNetLearner(X,y, hidden_layer_sizes=3, learning_rate=0.01, epochs=100)
+nNL = NeuralNetLearner(X,y,hidden_layer_sizes=[3])
 print(nNL([4.6, 3.1, 1.5, 0.2])) #0
 print(nNL([6.5, 3. , 5.2, 2. ])) #2
